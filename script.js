d7ec9a48c799b55845b440175e53c1559b997c84
unknown <greg.brosman@gmail.com>
2021-05-21 14:53:45 -0700

    form validation added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Variables for the inputs
+// Variables for the inputs. Grab the ID from the html.
 const form = document.getElementById('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -9,11 +9,36 @@ const password2 = document.getElementById('password2');
 function showError(input, message) {
     // Grab the parent element of the input (div with class of form-control)
     const formControl = input.parentElement;
-    // Change the class name
+    // Change the class name by overriding the html classname
     formControl.className = 'form-control error';
-    // Grab the small element and change the text to the passed in message
+    // Grab the small element and change the text to the passed in message. querySelector can take in a class, id, tag
     const small = formControl.querySelector('small');
     small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';  
+}
+
+// Check email is valid. test method returns true of false.
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// Check required fields. Use built in forEach method to loop through array.
+function checkRequired(inputArray) {
+    inputArray.forEach(function(input) {
+        // trim whitespace off
+        if (input.value.trim() === '') {
+            // input.id specifies the array value
+            showError(input, `${input.id} is required`); 
+        } else {
+            showSuccess(input);
+        }
+    })
 }
 
 
@@ -23,10 +48,6 @@ form.addEventListener('submit', function(e) {
     // Prevents from actual submitting
     e.preventDefault();
     
-    // If username is empty, display error message
-    if(username.value === '') {
-        showError(username, 'Username is required');
-    } else {
-        showSuccess(username);
-    }
+    // Create array of inputs and pass to the checkRequired function to be validated.
+    checkRequired([username, email, password, password2]);
 });
